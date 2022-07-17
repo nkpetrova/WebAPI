76b05a9870f171e0bd7b2b682bdfcf2f779a0ac5
Nadezhda Petrova <71227223+nkpetrova@users.noreply.github.com>
2022-07-17 23:13:57 +0300

    Add files via upload

---
+++ Services/ITurfirmaService.cs
@@ -0,0 +1,15 @@
+﻿using WebAPI.Domain;
+using WebAPI.Dto;
+
+namespace WebAPI.Services
+{
+    public interface ITurfirmaService
+    {
+        List<TurFirma> GetTurfirmas();
+        TurFirma GetTurfirmaById( int turfirmaId );
+        int CreateTurfirma(TurfirmaDto turfirmaDto );
+        void DeleteTurfirma( int turfirmaId);
+        List<Tuple<TurFirma, int>> GroupByAddress();
+        int UpdateTurfirma(TurfirmaDto turfirma);
+    }
+}
---
+++ Services/TurfirmaService.cs
@@ -0,0 +1,76 @@
+﻿using WebAPI.Domain;
+using WebAPI.Dto;
+using WebAPI.Repositories;
+
+namespace WebAPI.Services
+{
+    public class TurfirmaService : ITurfirmaService
+    {
+        private readonly ITurfirmaRepository _turfirmaRepository;
+
+        public TurfirmaService(ITurfirmaRepository turfirmaRepository)
+        {
+            _turfirmaRepository = turfirmaRepository;
+        }
+
+        public List<TurFirma> GetTurfirmas()
+        {
+            return _turfirmaRepository.GetAll();
+        }
+
+        public int CreateTurfirma( TurfirmaDto turfirma )
+        {
+            if (turfirma == null )
+            {
+                throw new Exception( $"{nameof( TurFirma )} is not found!" );
+            }
+
+            TurFirma turfirmaEntity = turfirma.ConvertToTurfirma();
+
+            return _turfirmaRepository.Create(turfirmaEntity);
+        }
+
+        public void DeleteTurfirma( int turfirmaId )
+        {
+            TurFirma turfirma = _turfirmaRepository.GetById( turfirmaId );
+            if (turfirma == null )
+            {
+                throw new Exception( $"{nameof(TurFirma)} not found, #Id - {turfirmaId}" );
+            }
+
+            _turfirmaRepository.Delete(turfirma);
+        }
+
+        public TurFirma GetTurfirmaById( int turfirmaId )
+        {
+            TurFirma turfirma = _turfirmaRepository.GetById(turfirmaId);
+            if (turfirma == null )
+            {
+                throw new Exception( $"{nameof(TurFirma)} not found, #Id - {turfirmaId}" );
+            }
+
+            return turfirma;
+        }
+
+        public List<Tuple<TurFirma, int>> GroupByAddress()
+        {
+            return _turfirmaRepository.GroupByAddress();
+        }
+
+        public int UpdateTurfirma(TurfirmaDto turfirma)
+        {
+            if (turfirma == null)
+            {
+                throw new Exception($"{nameof(TurFirma)} is not found!");
+            }
+            TurFirma turfirmaEntity = turfirma.ConvertToTurfirma();
+            TurFirma turfirmaTemp = _turfirmaRepository.GetById(turfirmaEntity.Id);
+            if (turfirmaTemp == null)
+            {
+                throw new Exception($"{nameof(TurFirma)} is not found!");
+            }
+            return _turfirmaRepository.Update(turfirmaEntity);
+        }
+
+    }
+}
