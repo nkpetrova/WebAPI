a4c76d14e84b66a2ed7dc4166b35aff21cbde3f8
Nadezhda Petrova <71227223+nkpetrova@users.noreply.github.com>
2022-07-17 23:09:06 +0300

    Add files via upload

---
+++ Controllers/TurfirmaController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Dto;
+using WebAPI.Services;
+
+namespace To_Do_List_Backend.Controllers
+{
+    [ApiController]
+    [Route( "rest/{controller}")]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITurfirmaService _turfirmaService;
+
+        public TodoController(ITurfirmaService turfirmaService)
+        {
+            _turfirmaService = turfirmaService;
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public IActionResult GetTurfirmas()
+        {
+            try
+            {
+                return Ok( _turfirmaService.GetTurfirmas()
+                    .ConvertAll( t => t.ConvertToTurfirmaDto() ) );
+            }
+            catch ( Exception ex )
+            {
+                return BadRequest( ex.Message );
+            }
+        }
+
+        [HttpGet]
+        [Route( "{turfirmaId}" )]
+        public IActionResult GetTurfirmaById( int turfirmaId )
+        {
+            try
+            {
+                return Ok( _turfirmaService.GetTurfirmaById(turfirmaId).ConvertToTurfirmaDto() );
+            }
+            catch (Exception ex)
+            {
+                return BadRequest( ex.Message );
+            }
+        }
+
+        [HttpPost]
+        [Route( "create" )]
+        public IActionResult CreateTurfirma( [FromBody] TurfirmaDto turfirma )
+        {
+            try
+            {
+                return Ok( _turfirmaService.CreateTurfirma(turfirma) );
+            }
+            catch ( Exception ex )
+            {
+                return BadRequest( ex.Message );
+            }
+        }
+
+        [HttpDelete]
+        [Route( "{turfirmaId}/delete" )]
+        public IActionResult DeleteTodo( int turfirmaId )
+        {
+            try
+            {
+                _turfirmaService.DeleteTurfirma(turfirmaId);
+                return Ok();
+            }
+            catch ( Exception ex )
+            {
+                return BadRequest( ex.Message );
+            }
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IActionResult UpdateTurfirma([FromBody] TurfirmaDto turfirma)
+        {
+            try
+            {
+                return Ok( _turfirmaService.UpdateTurfirma(turfirma));
+            }
+            catch ( Exception ex )
+            {
+                return BadRequest( ex.Message );
+            }
+        }
+
+        [HttpGet]
+        [Route("count_by_address")]
+        public IActionResult GroupByAddress()
+        {
+            try
+            {
+                return Ok(_turfirmaService.GroupByAddress());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
