4398056957a7ddc27a967a5fffbb163ca1c717b9
Nadezhda Petrova <71227223+nkpetrova@users.noreply.github.com>
2022-07-17 23:09:59 +0300

    Add files via upload

---
+++ Domain/Turfirma.cs
@@ -0,0 +1,18 @@
+﻿namespace WebAPI.Domain
+{
+    public class TurFirma
+    {
+        public int Id { get;  set; }
+        public string Name { get;  set; }
+        public string Address { get;  set; }
+        public int Commission { get;  set; }
+
+        public TurFirma(int id, string name, string address, int commission)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Commission = commission;
+        }
+    }
+}
