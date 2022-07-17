6c365ed40edb6541f5ad708d6749385d470378aa
Nadezhda Petrova <71227223+nkpetrova@users.noreply.github.com>
2022-07-17 23:11:32 +0300

    Add files via upload

---
+++ Dto/TurfirmaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Dto
+{
+    public class TurfirmaDto
+    {
+        public int Id { get;  set; }
+        public string Name { get;  set; }
+        public string Address { get;  set; }
+        public int Commission { get;  set; }
+    }
+}
---
+++ Dto/TurfirmaDtoExtensions.cs
@@ -0,0 +1,30 @@
+﻿using WebAPI.Domain;
+
+namespace WebAPI.Dto
+{
+    public static class TurfirmaDtoExtensions
+    {
+        public static TurFirma ConvertToTurfirma(this TurfirmaDto turfirmaDto )
+        {
+            return new TurFirma
+            (
+                turfirmaDto.Id,
+                turfirmaDto.Name,
+                turfirmaDto.Address,
+                turfirmaDto.Commission
+
+            );
+        }
+
+        public static TurfirmaDto ConvertToTurfirmaDto( this TurFirma turfirma )
+        {
+            return new TurfirmaDto
+            {
+                Id = turfirma.Id,
+                Name = turfirma.Name,
+                Address = turfirma.Address,
+                Commission = turfirma.Commission
+            };
+        }
+    }
+}
